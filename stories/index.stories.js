135d074f659a978b7ba7d796a3ef77ab7ec7ec1f
ericyip <letitbemine@gmail.com>
2018-01-03 17:33:57 +0800

    feat(button): add light, flat version

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,12 +4,17 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { withInfo } from '@storybook/addon-info';
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
+import {
+  withKnobs,
+  text,
+  boolean,
+  number,
+  select,
+} from '@storybook/addon-knobs/react';
 import { withNotes } from '@storybook/addon-notes';
+import { Welcome } from '@storybook/react/demo';
 
 import { Button, Label } from '../lib/';
-
-import { Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => (
   <Welcome showApp={linkTo('Button')} />
@@ -17,10 +22,113 @@ storiesOf('Welcome', module).add('to Storybook', () => (
 
 storiesOf('Button', module)
   .addDecorator(withKnobs)
-  .add('Primary', withInfo(`info`)(withNotes('This is note')(() => <Button>{text('Label', 'Hello Button')}</Button>)))
-  // .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  // .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+  .add(
+    'Default',
+    withInfo('Buttons default style')(
+      withNotes('This is note')(() => (
+        <Button
+          nature={select(
+            'Nature',
+            {
+              default: 'Default',
+              primary: 'Primary',
+              success: 'Success',
+              info: 'Info',
+              alarm: 'Alarm',
+              warning: 'Warning',
+            },
+            'default',
+          )}
+          size={select(
+            'Size',
+            {
+              xsmall: 'xsmall',
+              small: 'small',
+              medium: 'medium',
+              large: 'large',
+              xlarge: 'xlarge',
+            },
+            'medium',
+          )}>
+          {text('Label', 'Default Button')}
+        </Button>
+      )),
+    ),
+  )
+  .add(
+    'Flat',
+    withInfo('Buttons Flat style')(
+      withNotes('')(() => (
+        <Button
+          flat={boolean('Flat', true)}
+          nature={select(
+            'Nature',
+            {
+              default: 'Default',
+              primary: 'Primary',
+              success: 'Success',
+              info: 'Info',
+              alarm: 'Alarm',
+              warning: 'Warning',
+            },
+            'default',
+          )}>
+          {text('Label', 'Flat Button')}
+        </Button>
+      )),
+    ),
+  )
+  .add(
+    'Light',
+    withInfo('Button Light Style')(
+      withNotes('')(() => (
+        <Button
+          light={boolean('Light', true)}
+          nature={select(
+            'Nature',
+            {
+              default: 'Default',
+              primary: 'Primary',
+              success: 'Success',
+              info: 'Info',
+              alarm: 'Alarm',
+              warning: 'Warning',
+            },
+            'default',
+          )}>
+          {text('Label', 'Light Button')}
+        </Button>
+      )),
+    ),
+  )
+  .add(
+    'Disable',
+    withInfo('Buttons disable style')(
+      withNotes('')(() => (
+        <Button disabled={boolean('Disabled', true)}>
+          {text('Label', 'Disable Button')}
+        </Button>
+      )),
+    ),
+  )
+  .add(
+    'Link',
+    withInfo('Link style')(
+      withNotes('')(() => (
+        <Button link>{text('Label', 'Disable Button')}</Button>
+      )),
+    ),
+  );
+// .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+// .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
 
-storiesOf('Label', module)
-  .addDecorator(withKnobs)
-  .add('Input Label', withInfo(`info`)(withNotes('This is label')(() => <Label>Hello {text('Label', 'Eric')}</Label>)))
+// storiesOf('Label', module)
+//   .addDecorator(withKnobs)
+//   .add(
+//     'Input Label',
+//     withInfo(`info`)(
+//       withNotes('This is label')(() => (
+//         <Label>Hello {text('Label', 'Eric')}</Label>
+//       )),
+//     ),
+//   );
