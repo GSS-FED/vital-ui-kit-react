81974a57ac68a9fee965cd5facd15160450429f2
ericyip <letitbemine@gmail.com>
2018-04-07 21:39:08 +0800

    [native] Add Button

--- packages/native/storybook/stories/index.js
+++ packages/native/storybook/stories/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Text } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import Button from './Button';
+import { Button } from '../../src';
 import CenterView from './CenterView';
 import Welcome from './Welcome';
 
@@ -14,12 +13,5 @@ storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo(
 storiesOf('Button', module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
   .add('with text', () => (
-    <Button onPress={action('clicked-text')}>
-      <Text>Hello Button!!</Text>
-    </Button>
+    <Button onPress={action('clicked-text')} label="Hello Button" />
   ))
-  .add('with some emoji', () => (
-    <Button onPress={action('clicked-emoji')}>
-      <Text>😀 😎 👍 💯</Text>
-    </Button>
-  ));
