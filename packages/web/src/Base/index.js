2abf942fb08e2186cec5d303ba2150d9aaf9a552
Eric Yip <letitbemine@gmail.com>
2018-06-01 15:51:40 +0800

    Fix rename base git

--- packages/web/src/Base/index.js
+++
@@ -1,7 +0,0 @@
-import { colorPaletteGenerator } from './theme/helper';
-
-export { default as styles } from './theme';
-export { default as ThemeProvider } from './ThemeProvider';
-export {
-  colorPaletteGenerator
-}
