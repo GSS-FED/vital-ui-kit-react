8da2ad2df4e6971b2da8484f328a5a58a721c335
Eric Yip <letitbemine@gmail.com>
2019-01-17 13:21:35 +0800

    Use ts-jest preset

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,7 @@
-const TEST_REGEX =
-  '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$';
-
 module.exports = {
-  testRegex: TEST_REGEX,
+  preset: 'ts-jest/presets/js-with-babel',
+  testEnvironment: 'node',
   setupTestFrameworkScriptFile: './jest.setup.js',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-    '^.+\\.md?$': 'markdown-loader-jest',
-  },
   testPathIgnorePatterns: [
     '<rootDir>/examples/',
     '<rootDir>/node_modules/',
@@ -18,11 +11,5 @@ module.exports = {
     '\\.(css|less)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'identity-obj-proxy',
-  },
-  globals: {
-    'ts-jest': {
-      useBabelrc: true,
-      // tsConfigFile: 'tsconfig.json',
-    },
   },
 };
