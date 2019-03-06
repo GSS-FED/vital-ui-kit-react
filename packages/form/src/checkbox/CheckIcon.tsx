92f118261dcaba5f52cb5124c4091b0284c0c61a
cliff <cliff_chi@gss.com.tw>
2019-03-06 17:18:36 +0800

    fix checkbox icon size
    defaultTheme cant change so use themeConsumer

--- packages/form/src/checkbox/CheckIcon.tsx
+++ packages/form/src/checkbox/CheckIcon.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ThemeConsumer } from 'styled-components';
 
 type Props = {
   fontSize?: number | string;
@@ -6,19 +7,23 @@ type Props = {
   size?: string;
 };
 
-export const CheckIcon = ({ size, fontSize, ...props }: Props) => (
-  <svg
-    role="img"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 512 512"
-    width={size}
-    height={size}
-    fontSize={fontSize}
-    {...props}
-  >
-    <path
-      fill="currentColor"
-      d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"
-    />
-  </svg>
+export const CheckIcon = ({ fontSize, ...props }: Props) => (
+  <ThemeConsumer>
+    {theme => (
+      <svg
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        width={theme.checkbox.icon.size}
+        height={theme.checkbox.icon.size}
+        fontSize={fontSize}
+        {...props}
+      >
+        <path
+          fill="currentColor"
+          d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"
+        />
+      </svg>
+    )}
+  </ThemeConsumer>
 );
