f9276d6d28b114bdcb188cb7ae334d7d8d1cf5ac
Eric Yip <letitbemine@gmail.com>
2019-03-08 17:14:44 +0800

    Update config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,77 +1,22 @@
-// const path = require('path');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-// module.exports = ({ config, mode }) => {
-//   //babel-loader@8 installed and @babel/core@7
-//   config.module.rules[0].use[0].loader = require.resolve(
-//     'babel-loader',
-//   );
-
-//   config.module.rules[0].use[0].options.presets = [
-//     require.resolve('@babel/preset-react'),
-//     require.resolve('@babel/preset-env'),
-//   ];
-
-//   // any plugin you want to add
-//   config.module.rules[0].use[0].options.plugins = [
-//     require.resolve('@babel/plugin-proposal-object-rest-spread'),
-//     require.resolve('babel-plugin-styled-components'),
-//   ];
-
-//   config.module.rules.push({
-//     test: /\.(ts|tsx)$/,
-//     use: [
-//       {
-//         loader: require.resolve('babel-loader'),
-//         options: {
-//           babelrc: false,
-//           presets: [
-//             require.resolve('@babel/preset-react'),
-//             require.resolve('@babel/preset-env'),
-//           ],
-//           plugins: [
-//             require.resolve(
-//               '@babel/plugin-proposal-object-rest-spread',
-//             ),
-//             require.resolve('babel-plugin-styled-components'),
-//           ],
-//         },
-//       },
-//       {
-//         loader: 'ts-loader',
-//         options: {
-//           transpileOnly: true,
-//         },
-//       },
-//       { loader: 'react-docgen-typescript-loader' },
-//       // {
-//       //   loader: require.resolve("awesome-typescript-loader"),
-//       //   options: {
-//       //     useCache: true,
-//       //     useBabel: true,
-//       //     configFileName: 'tsconfig.storybook.json',
-//       //     babelCore: '@babel/core',
-//       //   },
-//       // },
-//       {
-//         loader: require.resolve(
-//           '@storybook/addon-storysource/loader',
-//         ),
-//         options: {
-//           parser: 'typescript',
-//         },
-//       },
-//     ],
-//   });
-//   config.resolve.plugins = [
-//     new TsconfigPathsPlugin({
-//       configFile: 'tsconfig.storybook.json',
-//     }),
-//   ];
-//   config.resolve.extensions.push('.ts', '.tsx', '.json');
-//   return config;
-// };
 const path = require('path');
-module.exports = ({ config }) => {
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+module.exports = ({ config, mode }) => {
+  //babel-loader@8 installed and @babel/core@7
+  config.module.rules[0].use[0].loader = require.resolve(
+    'babel-loader',
+  );
+
+  config.module.rules[0].use[0].options.presets = [
+    require.resolve('@babel/preset-react'),
+    require.resolve('@babel/preset-env'),
+  ];
+
+  // any plugin you want to add
+  config.module.rules[0].use[0].options.plugins = [
+    require.resolve('@babel/plugin-proposal-object-rest-spread'),
+    require.resolve('babel-plugin-styled-components'),
+  ];
+
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
@@ -80,25 +25,40 @@ module.exports = ({ config }) => {
         options: {
           babelrc: false,
           presets: [
-            [
-              'react-app',
-              {
-                flow: false,
-                typescript: true,
-              },
-            ],
+            require.resolve('@babel/preset-react'),
+            require.resolve('@babel/preset-env'),
           ],
-          plugins: ['babel-plugin-styled-components'],
+          plugins: [
+            require.resolve(
+              '@babel/plugin-proposal-object-rest-spread',
+            ),
+            require.resolve('babel-plugin-styled-components'),
+          ],
         },
       },
       {
-        loader: require.resolve('react-docgen-typescript-loader'),
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
       },
+      { loader: 'react-docgen-typescript-loader' },
+      // {
+      //   loader: require.resolve("awesome-typescript-loader"),
+      //   options: {
+      //     useCache: true,
+      //     useBabel: true,
+      //     configFileName: 'tsconfig.storybook.json',
+      //     babelCore: '@babel/core',
+      //   },
+      // },
       {
         loader: require.resolve(
           '@storybook/addon-storysource/loader',
         ),
-        options: { parser: 'typescript' },
+        options: {
+          parser: 'typescript',
+        },
       },
     ],
   });
@@ -107,6 +67,46 @@ module.exports = ({ config }) => {
       configFile: 'tsconfig.storybook.json',
     }),
   ];
-  config.resolve.extensions.push('.ts', '.tsx');
+  config.resolve.extensions.push('.ts', '.tsx', '.json');
   return config;
 };
+// const path = require('path');
+// module.exports = ({ config }) => {
+//   config.module.rules.push({
+//     test: /\.(ts|tsx)$/,
+//     use: [
+//       {
+//         loader: require.resolve('babel-loader'),
+//         options: {
+//           babelrc: false,
+//           presets: [
+//             [
+//               'react-app',
+//               {
+//                 flow: false,
+//                 typescript: true,
+//               },
+//             ],
+//           ],
+//           plugins: ['babel-plugin-styled-components'],
+//         },
+//       },
+//       {
+//         loader: require.resolve('react-docgen-typescript-loader'),
+//       },
+//       {
+//         loader: require.resolve(
+//           '@storybook/addon-storysource/loader',
+//         ),
+//         options: { parser: 'typescript' },
+//       },
+//     ],
+//   });
+//   config.resolve.plugins = [
+//     new TsconfigPathsPlugin({
+//       configFile: 'tsconfig.storybook.json',
+//     }),
+//   ];
+//   config.resolve.extensions.push('.ts', '.tsx');
+//   return config;
+// };
