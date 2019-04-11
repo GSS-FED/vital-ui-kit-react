3cac6b579bdf8c60d49880e7a13e4877890fd3ab
Eric Yip <letitbemine@gmail.com>
2019-04-11 16:15:18 +0800

    Box story

--- packages/utils/src/box/Box.stories.tsx
+++ packages/utils/src/box/Box.stories.tsx
@@ -4,7 +4,14 @@ import { storiesOf } from '@storybook/react';
 import { Box } from '.';
 
 storiesOf('utils | Box', module).add('Basic', () => (
-  <Box m="30px" p="20px" maxWidth="200px" width="90%" height="500px">
+  <Box
+    color="red"
+    m="30px"
+    p="20px"
+    maxWidth="200px"
+    width="90%"
+    height="500px"
+  >
     This is a box!
   </Box>
 ));
