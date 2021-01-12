66c096f488092c30c093ee0fe6ab35acf8b50da5
Isaac Huang <isaac_huang@gss.com.tw>
2021-01-12 11:05:50 +0800

    chore: remove rollup plugins: commonjs and node-resolve
    Because we external every dependencies.

--- packages/popup/rollup.config.js
+++ packages/popup/rollup.config.js
@@ -1,28 +1,23 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import autoExternal from 'rollup-plugin-auto-external';
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 
 const config = {
-  external: [
-    'react',
-    'react-dom',
-    'styled-components',
-  ],
+  external: ['react', 'react-dom', 'styled-components'],
   input: 'src/index.ts',
-  output: [{
-    file: 'dist/index.js',
-    format: 'esm',
-    sourcemap: true,
-  }, {
-    file: 'cjs/index.js',
-    format: 'cjs',
-    sourcemap: true,
-  }],
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: 'cjs/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+  ],
   plugins: [
-    resolve(),
-    commonjs(),
     autoExternal(),
     typescript({ useTsconfigDeclarationDir: true }),
     postcss(),
@@ -30,4 +25,3 @@ const config = {
 };
 
 export default config;
-
--- packages/tooltip/rollup.config.js
+++ packages/tooltip/rollup.config.js
@@ -1,23 +1,22 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import autoExternal from 'rollup-plugin-auto-external';
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 
 const config = {
   input: 'src/index.ts',
-  output: [{
-    file: 'dist/index.js',
-    format: 'esm',
-    sourcemap: true,
-  }, {
-    file: 'cjs/index.js',
-    format: 'cjs',
-    sourcemap: true,
-  }],
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: 'cjs/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+  ],
   plugins: [
-    resolve(),
-    commonjs(),
     autoExternal(),
     typescript({ useTsconfigDeclarationDir: true }),
     postcss(),
@@ -25,4 +24,3 @@ const config = {
 };
 
 export default config;
-
