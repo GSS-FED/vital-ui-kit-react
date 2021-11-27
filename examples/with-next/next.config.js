1dc6b70881c401eb2cacb4da57b22d284bc07f25
Isaac Huang <isaac_huang@gss.com.tw>
2021-11-28 00:13:11 +0800

    fix: use webpack4 in the next example

--- examples/with-next/next.config.js
+++ examples/with-next/next.config.js
@@ -1,11 +1,16 @@
+const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
+
+const nextConfig = {
+  webpack5: false,
+};
 
 // fix: prevents error when .css files are required by node
 if (typeof require !== 'undefined') {
-  require.extensions['.css'] = file => {};
+  require.extensions['.css'] = (file) => {};
 }
 /* Without CSS Modules, with PostCSS */
-module.exports = withCSS();
+module.exports = withPlugins([[withCSS()]], nextConfig);
 
 /* With CSS Modules */
 // module.exports = withCSS({ cssModules: true })
